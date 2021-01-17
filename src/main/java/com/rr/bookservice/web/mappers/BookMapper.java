af271c26f4776dce12f54d9a14bcd93f410d86f2
Ravi Ravula <rr@test.com>
2021-01-17 17:15:15 -0500

    configure mapstruct and implement an example use case. closes #11

---
+++ src/main/java/com/rr/bookservice/web/mappers/BookMapper.java
@@ -0,0 +1,13 @@
+package com.rr.bookservice.web.mappers;
+
+import com.rr.bookservice.domain.Book;
+import com.rr.bookservice.web.model.BookDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BookMapper {
+
+    BookDto bookToBookDto(Book book);
+
+    Book bookDtoToBook(BookDto bookDto);
+}
