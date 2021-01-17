76ce9cf52c7504aff163428899aea8bc4b31e4d7
Ravi Ravula <rr@test.com>
2021-01-16 20:27:16 -0500

    create JPA entity for book objects. closes #3

---
+++ src/main/java/com/rr/bookservice/domain/Book.java
@@ -0,0 +1,42 @@
+package com.rr.bookservice.domain;
+
+import com.sun.javafx.beans.IDProperty;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String bookName;
+    private String genre;
+
+    @Column(unique = true)
+    private Long isbn;
+}
