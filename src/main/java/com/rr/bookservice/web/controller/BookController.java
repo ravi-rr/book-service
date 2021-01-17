9bf5f8a24c757b9d458a525680b2096945c25999
Ravi Ravula <rr@test.com>
2021-01-17 00:27:25 -0500

    implement global exception handling using controller advice. closes #9

--- src/main/java/com/rr/bookservice/web/controller/BookController.java
+++ src/main/java/com/rr/bookservice/web/controller/BookController.java
@@ -56,16 +56,4 @@ public class BookController {
     public void deleteBook(@PathVariable("bookId") UUID bookId) {
         bookService.deleteById(bookId);
     }
-
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 }
